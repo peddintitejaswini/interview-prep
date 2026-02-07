6c56114d41e78a9c3fd8b06fca4cf73addec9245
Yaswanth Varada <yaswanthvarada20@gmail.com>
2026-02-07 11:02:41 +0530

    Google Generative AI bug fixed and Font changed

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -8,7 +8,7 @@ const apiKey = import.meta.env.VITE_GEMINI_API_KEY!;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash-exp",
+  model: "gemini-2.5-flash",
 });
 
 const generationConfig = {
